00c8e537657ce372cedbac12af6f8763556bb1ee
dramhex <92268356+dramhex@users.noreply.github.com>
2025-06-12 14:32:04 +0200

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from utils.data_processor import process_dataset
 
 def main():
-    file_path = 'data/weatherHistory.csv'
-    #file_path = 'data/sample_salary_dataset.csv'
+    #file_path = 'data/weatherHistory.csv'
+    file_path = 'data/sample_salary_dataset.csv'
     
     process_dataset(file_path)
 
