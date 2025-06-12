bf6dcf7c78953396dbf853ea724721360e748430
dramhex <92268356+dramhex@users.noreply.github.com>
2025-06-12 16:58:09 +0200

    chore: update code structure and remove redundant sections

--- utils/data_processor.py
+++ utils/data_processor.py
@@ -1,34 +1,35 @@
 import os
 from utils.file_loader import load_csv
-from settings import (
-    get_columns, set_features, set_target,
-    select_model
-)
+from settings import settings_instance as settings
+
 from utils.visualization import plot_scatter, plot_3d, save_plot 
 from models.regression import train_models
 
 def process_dataset(file_path: str):
     df = load_csv(file_path)
-    print(df.head())
-
     dataset_name = os.path.splitext(os.path.basename(file_path))[0]
-    model_type = select_model()
-    columns = get_columns(df)
+    columns = settings.get_columns(df)
 
     if not columns:
         print("No valid columns available for the selected model.")
         return
 
-    features = set_features(columns, model_type)
-    target = set_target(columns, features, model_type)
+    model_type = settings.select_model()
+
+    
+
+    target = settings.set_target()
+    features = settings.set_features()
 
     print(f'Selected features: {features}')
     print(f'Selected target: {target}')
 
+    settings.display_settings
+
     models, splits = train_models(df, features, target, model_type)
 
     # Visualisation
-    if model_type == "simple_linear":
+    if model_type == "linear_regression":
         user_input = input('Do you want to visualize the data? (yes/no): ').strip().lower()
         if user_input == 'yes' and len(features) > 0:
             fig = plot_scatter(df, features, target, models=models)
